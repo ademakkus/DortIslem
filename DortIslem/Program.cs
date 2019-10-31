4c3277bfbd249cb83d954dd273d579d05e92d12e
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2019-10-31 21:42:54 +0300

    Kod eklendi

---
+++ DortIslem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  static  System.Console;
+
+namespace DortIslem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kullaniciSecimi = -1;
+            while (kullaniciSecimi!=0)
+            {
+                kullaniciSecimi = MenuGoster();
+
+            }
+            Write(kullaniciSecimi);
+            ReadKey();
+        }
+
+        private static int MenuGoster()
+        {
+            Console.WriteLine("1- Toplama İşlemi\n" +
+                              "2- Çıkarma İşlemi\n" +
+                              "3- Çarpma İşlemi\n" +
+                              "4- Bölme İşlemi \n" +
+                              "5 - Mod Alma İşlemi");
+            int secim = int.Parse(ReadLine());
+            return secim;
+        }
+    }
+}
